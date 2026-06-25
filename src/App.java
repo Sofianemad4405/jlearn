c33c51e52ac5bae23a2e515f5c2c8c7d380455b5
Sofianemad4405 <sofianemad98@gmail.com>
2026-06-25 23:18:46 +0300

    Add user input handling and formatting output in App class

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
         //printing
@@ -46,7 +48,45 @@ public class App {
         System.out.println("x after *= 2: " + x);
         x /= 4; // x = x / 4
         System.out.println("x after /= 4: " + x);
-
         
+        //printf
+        String name="koko";
+        int myAge=21;
+        float salary=3.14f;
+        System.out.printf("my name is %s and my age is %d and my salary is %.2f", name, myAge, salary);
+
+        //scanner
+        Scanner scanner =new Scanner(System.in);
+        String userName=scanner.nextLine();
+        System.out.println("Hello, " + userName);
+        scanner.close();
+
+        //Ternary Operators, Switch Case 
+        int number=2;
+        String result= (number % 2 == 0) ? "even" : "odd";
+        System.out.println("The number is " + result);
+        
+        //scanner +switch
+        char grade =scanner.next().charAt(0);
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent");
+                break;
+            case 'B':
+                System.out.println("Good");
+                break;
+            case 'C':
+                System.out.println("Average"); 
+                break;
+            case 'D':
+                System.out.println("Below Average");
+                break;
+            case 'F':
+                System.out.println("Fail");
+                break;
+            default:
+                System.out.println("Invalid grade");
+                break;
+        }
      }
 }

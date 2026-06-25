48650573ebb84488911488c80535c6f449ab4071
Sofianemad4405 <sofianemad98@gmail.com>
2026-06-25 19:19:29 +0300

    Add initial implementation of App class with main method and output statements

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, koko1");
+        System.out.println("Hello, koko2");
+        System.out.println("Hello, koko3");
+        System.out.println();
+        int age=21;
+        String myName="sofian";
+        System.out.println("my name is " + myName + " and my age is " + age);
+        float myFloat=3.14f;
+        double myDouble=3.14;
+        System.out.println("my float is " + myFloat + " and my double is " + myDouble);
+    }
+}
